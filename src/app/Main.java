a76b392ceeb922206ba65d70d92f8db9d0f88a80
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-05-17 20:02:46 -0600

    Update Main.java
    Código de prueba de funcionalidad

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,9 +6,20 @@ public class Main {
         
         graph.addVertex("Vertex1");
         graph.addVertex("Vertex2");
+        graph.addVertex("Vertex3");
 
         graph.addEdge("Vertex1", "Vertex2", 1);
+        graph.addEdge("Vertex2", "Vertex3", 2);
+        graph.addEdge("Vertex1", "Vertex3", 3);
+
+        // graph.removeEdge("Vertex1", "Vertex2");
+        // graph.removeVertex("Vertex2");
 
         graph.printGraph();
+        graph.printAdjVertices();
+
+        System.out.println("Adjacency Matrix:");
+        graph.printAdjacencyMatrix();
+
     }
 }
